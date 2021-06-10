9a37856e9203752cb7ea609660fe398d52304527
James Chamberlain <james@chamber-made.com>
2021-06-10 21:24:53 +0200

    feat(usexmlresponse.tsx): add local storage with asyncstorage

--- hooks/useXMLResponse.tsx
+++ hooks/useXMLResponse.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { parse } from 'fast-xml-parser';
 import { Feed } from '../types/articles.types';
 
@@ -9,6 +10,25 @@ export type XMLResponseProps = {
 export function useXMLResponse({ url }: XMLResponseProps): Feed | null {
   const [parsedFeed, setParsedFeed] = useState<Feed | null>(null);
 
+  async function storeLocalData(data: Feed): Promise<void> {
+    try {
+      const stringData = JSON.stringify(data);
+      await AsyncStorage.setItem('@storage_Key', stringData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function getLocalData(): Promise<void> {
+    try {
+      const jsonValue = await AsyncStorage.getItem('storage_Key');
+      const parsedValue = jsonValue ? JSON.parse(jsonValue) : null;
+      setParsedFeed(parsedValue);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     const controller = new AbortController();
     const { signal } = controller;
@@ -16,7 +36,14 @@ export function useXMLResponse({ url }: XMLResponseProps): Feed | null {
     fetch(url, { signal })
       .then((response: Response) => response.text())
       .then((textResponse: string) => parse(textResponse, {}, true))
-      .then((parsedResponse: Feed) => setParsedFeed(parsedResponse));
+      .then((parsedResponse: Feed) => {
+        setParsedFeed(parsedResponse);
+        storeLocalData(parsedResponse);
+      });
+
+    if (!parsedFeed) {
+      getLocalData();
+    }
 
     return function cancel() {
       controller.abort();
